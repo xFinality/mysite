51fec428aa974af45f636a0a76b5e6172f2a5e87
Thomas Merat <phenix25@hotmail.fr>
2019-01-29 23:55:35 +0100

    Starting components for template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
+import Slider from './components/Slider/Slider';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App vh-100">
+        <Navigation />
+        <Slider />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Slider= () => {
+	return (
+		<div className='bt bb vh-50'>
+		test
+		</div>
+		)
+}
+
+export default Slider;
