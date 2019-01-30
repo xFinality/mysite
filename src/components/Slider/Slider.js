e22d0dbdfbafb184f2d735d722c0a989afe83e7f
Thomas Merat <phenix25@hotmail.fr>
2019-01-30 20:21:09 +0100

    Separating the arrows

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './Slider.css';
+import Arrow from '../Arrow/Arrow';
 
 const Slider= () => {
 	return (
 		<div className='flex flex-column  vh-50 '>
 			<div className='flex items-center justify-center bt bb h-95 w-100 overflow-hidden'>
-				<div className='w-button h-100 flex flex-column items-center justify-center'>
-					<div className='arrow button-prev'></div>
-				</div>
+				<Arrow direction='button-prev'/>
 				<div className='w-50 h-100'>test</div>
-				<div className='w-button h-100 flex flex-column items-center justify-center'>
-					<div className='arrow button-next'></div>
-				</div>
+				<Arrow direction='button-next' />
 			</div>
 			<div className='flex items-center justify-center bt bb w-100 h-5 mv0 pv0'>
 				<div className='mr2 circle'></div>
