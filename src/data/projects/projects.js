2e9f057e3aea7fc37a7f13b8d7084343d9fac78e
Thomas Merat <phenix25@hotmail.fr>
2019-03-01 20:36:54 +0100

    added link

--- src/data/projects/projects.js
+++ src/data/projects/projects.js
@@ -5,21 +5,24 @@ export default {
 			"name" : "My Site",
 			"description" : "This is this very website. I made it from scratch using these skills.",
 			"imageId" : "1",
-			"skill" : ["html5","css3","js","react","nodejs"]
+			"skill" : ["html5","css3","js","react","nodejs"],
+			"git" : "https://github.com/xFinality/mysite"
 		},
 		{
 			"id": "2", 
 			"name" : "Face recognition App",
 			"description" : "This is an app that allow the user to register, login and then with the link of an image, show the faces.",
 			"imageId" : "2",
-			"skill" : ["html5","css3","js","react","nodejs","clarifai","redux"]
+			"skill" : ["html5","css3","js","react","nodejs","clarifai","redux"],
+			"git" : "https://github.com/xFinality/Face-Recognition-app"
 		},
 		{
 			"id": "3", 
 			"name" : "Robot App",
 			"description" : "This is a small app that uses an api to dynamically research info within cards",
 			"imageId" : "3",
-			"skill" : ["html5","css3","js","react","robohash"]
+			"skill" : ["html5","css3","js","react","robohash"],
+			"git" : "https://github.com/xFinality/RoboFriends"
 		},		
 	]
 };
